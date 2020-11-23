0c7e0d6b9c0e7c51813c8479582553f7ff764d80
Stony Chen <stonychen@hotmail.com>
2020-11-23 22:48:42 +0800

    update

--- src/test/java/com/sample/spring/SampleApplicationTests.java
+++ src/test/java/com/sample/spring/SampleApplicationTests.java
@@ -1,5 +1,8 @@
 package com.sample.spring;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,8 +10,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 class SampleApplicationTests {
 
 	@Test
+	@DisplayName("num should be equal num2.")
 	void contextLoads() {
-		
+		Integer num = 11;
+		Integer num2 = 11;
+ 
+		assertEquals(num, num2);
 	}
 
 }
