a32170760bb9d02867a566d9a5c746301194d622
Stony Chen <stonychen@hotmail.com>
2020-11-23 21:34:54 +0800

    Initial commit

---
+++ src/main/java/com/samplejavamaven/samplejavamaven/SampleJavaMavenApplication.java
@@ -0,0 +1,13 @@
+package com.samplejavamaven.samplejavamaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleJavaMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleJavaMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samplejavamaven/samplejavamaven/SampleJavaMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samplejavamaven.samplejavamaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleJavaMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
